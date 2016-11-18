8ed0d8b5b82e688fc3f40174524400e8dbce7703
RachaelMahon <rachmahon@gmail.com>
2016-11-18 17:52:51 +0000

    Friday commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
   class User < ApplicationRecord
-    has_many :restaurants
-    has_many :reviews
+    has_many :restaurant
+    has_many :review
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
-
-
 
      def self.from_omniauth(auth)
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
